752ddadd48d70034a72399218c82507d1a68c51f
AZHARUL_ISLAM <azharul.islam@moveon.com.bd>
2025-02-09 10:41:52 +0600

    create api endpoints

---
+++ lib/core/network/api_endpoints.dart
@@ -0,0 +1,14 @@
+
+import 'api_config.dart';
+
+class APIEndPoints {
+
+  static const String kCampaignsEndPoint =
+      '${APIConfig.kBaseUrl}/api/campaigns';
+
+  static const String kCampaignItemListEndPoint =
+      '${APIConfig.kBaseUrl}/api/campaigns/{campaignId}/listing';
+
+
+
+}
