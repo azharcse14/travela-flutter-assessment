df7bba9a805127e208cf3d1f17c8823c12eb76f7
AZHARUL_ISLAM <azharul.islam@moveon.com.bd>
2025-02-09 10:41:43 +0600

    create api config

---
+++ lib/core/network/api_config.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+
+const String APPLICATION_JSON = "application/json";
+const String CONTENT_TYPE = "content-type";
+const String ACCEPT = "accept";
+
+class APIConfig {
+  static const String kBaseUrl = 'https://api.travela.xyz/';
+}
