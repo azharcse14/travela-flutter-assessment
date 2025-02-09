f83afa610bbc96ed41cda607799656ad1328ec61
AZHARUL_ISLAM <azharul.islam@moveon.com.bd>
2025-02-09 10:47:28 +0600

    create asset manager

---
+++ lib/core/utility/constants/asset_manger.dart
@@ -0,0 +1,24 @@
+const String IMAGE_PATH = "assets/images";
+
+class ImageAssets {
+
+  ///NAVBAR
+  static String navExploreIcon = '$IMAGE_PATH/nav_icon/explore_icon.svg';
+  static String navBookingIcon = '$IMAGE_PATH/nav_icon/booking_icon.svg';
+  static String navInboxIcon = '$IMAGE_PATH/nav_icon/inbox_icon.svg';
+  static String navProfileIcon = '$IMAGE_PATH/nav_icon/profile_icon.svg';
+  static String navHostIcon = '$IMAGE_PATH/nav_icon/host_icon.svg';
+
+  static String placeHolderSvgImage = '$IMAGE_PATH/Placeholder_view.svg';
+
+  static String placeHolderNetworkImage =
+      'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png';
+
+
+  static String noDataAvailableIcon =
+      '$IMAGE_PATH/svg/no_data_available_icon.svg';
+  static String noInternetConnectionIcon =
+      '$IMAGE_PATH/svg/no_internet_connection_icon.svg';
+  static String noDataFoundIcon = '$IMAGE_PATH/svg/no_data_found_icon.svg';
+  static String sessionTimeoutIcon = '$IMAGE_PATH/svg/session_timeout_icon.svg';
+}
