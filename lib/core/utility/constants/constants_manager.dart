d0e35eab1bba200dae47a89d402fe080feae130f
AZHARUL_ISLAM <azharul.islam@moveon.com.bd>
2025-02-09 11:13:23 +0600

    feat: add constant

---
+++ lib/core/utility/constants/constants_manager.dart
@@ -0,0 +1,24 @@
+class AppConstants {
+  static const int splashDelay = 800;
+  static const int onBoardingImage = 600;
+  static const int sliderAnimationTime = 300;
+  static const double defaultBannerRatio = 2.488888888888889;
+  static const Duration apiTimeOut = Duration(minutes: 1);
+  static const String VAPID_KEY = "KKg8HRQupReZw3b0cGwM_8_xTCuHvA5weNJpDkcFq_U";
+  static const String notification_channel_id = 'channel_id';
+  static const String notification_channel_title =
+      'High Importance Notifications';
+  static const String notification_channel_descroption =
+      'This channel is used for important notifications.';
+  static const String notification_icon = '@drawable/ic_launcher';
+
+  // static const String authorizationKey2 = ""; //ApiKey from Novu
+  static const String providerId = "fcm";
+  static const String contactUsLink = "https://moveon.global/AE_en";
+}
+
+// Animation
+// const Duration kButtonAnimationDuration = Duration(milliseconds: 600);
+// const Duration kCardAnimationDuration = Duration(milliseconds: 400);
+// const Duration kRippleAnimationDuration = Duration(milliseconds: 400);
+// const Duration kLoginAnimationDuration = Duration(milliseconds: 1500);
---
+++ lib/core/utility/constants/string_manager.dart
@@ -0,0 +1,20 @@
+class AppStrings {
+  static const strNoRouteFound = "no_route_found";
+  static const strAppName = "app_name";
+
+
+  static const String success = "success";
+  // error handler
+  static const String strBadRequestError = "Bad Request";
+  static const String strNoContent = "No content";
+  static const String strForbiddenError = "Forbidden";
+  static const String strUnauthorizedError = "Unauthorized";
+  static const String strNotFoundError = "Data Not Found";
+  static const String strConflictError = "Conflict Error";
+  static const String strInternalServerError = "Internal Server Error";
+  static const String strUnknownError = "Unknown Error";
+  static const String strTimeoutError = "Timeout Error";
+  static const String strDefaultError = "default_error";
+  static const String strCacheError = "Cache Error";
+  static const String strNoInternetError = "no_internet_error";
+}
