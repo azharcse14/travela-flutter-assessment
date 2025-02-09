7276f675ec1685fd8b7ee1b81d4cde6d90d82b9a
AZHARUL_ISLAM <azharul.islam@moveon.com.bd>
2025-02-09 10:48:20 +0600

    create explore api service

---
+++ lib/features/explore/data/api_service/explore_api_service.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+import 'package:travela_assignment/core/network/api_endpoints.dart';
+import 'package:travela_assignment/features/explore/data/models/campaign_item_list_model.dart';
+import 'package:travela_assignment/features/explore/data/models/campaigns_model.dart';
+
+part 'explore_api_service.g.dart';
+
+@RestApi()
+abstract class ExploreApiService {
+  factory ExploreApiService(Dio dio) {
+    return _ExploreApiService(dio);
+  }
+
+  @GET(APIEndPoints.kCampaignsEndPoint)
+  Future<HttpResponse<CampaignsModel>> getCampaigns();
+
+  @GET(APIEndPoints.kCampaignItemListEndPoint)
+  Future<HttpResponse<CampaignItemListModel>> getCampaignItemList({
+    @Path() required int campaignId,
+    @Query("page") required int page,
+    @Query("per_page") required int perPage,
+  });
+
+}
